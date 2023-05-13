2b55c44063fcab09cdd588be5d09997b261ae0c9
Jaime-am <46279145+Jaime-am@users.noreply.github.com>
2023-05-13 20:48:01 +0200

    Update gen_code.sh

--- gen_code.sh
+++ gen_code.sh
@@ -9,5 +9,6 @@ key_dec_aud=$(echo $key_aud | xxd -r -p | base64)
 sudo mkdir /var/www/html/$(echo $user)
 bash -c "/Bento4-SDK-1-6-0-639.x86_64-unknown-linux/bin/mp4encrypt --method MPEG-CENC --key 1:$(echo $key_vid):random --property 1:KID:$(echo $key_aud) --global-option mpeg-cenc.eme-pssh:true /var/www/html/videos/low_config.mp4 /var/www/html/$(echo $user)/low-config-enc.mp4
 bash -c "/Bento4-SDK-1-6-0-639.x86_64-unknown-linux/bin/mp4encrypt --method MPEG-CENC --key 1:$(echo $key_vid):random --property 1:KID:$(echo $key_aud) --global-option mpeg-cenc.eme-pssh:true /var/www/html/videos/med_config.mp4 /var/www/html/$(echo $user)/med-config-enc.mp4
-bash -c "/Bento4-SDK-1-6-0-639.x86_64-unknown-linux/bin/mp4encrypt --method MPEG-CENC --key 1:$(echo $key_vid):random --property 1:KID:$(echo $key_aud) --global-option mpeg-cenc.eme-pssh:true /var/www/html/videos/high_config.mp4 /var/www/html/$(echo $user)/high-config-fragment-enc.mp4
+bash -c "/Bento4-SDK-1-6-0-639.x86_64-unknown-linux/bin/mp4encrypt --method MPEG-CENC --key 1:$(echo $key_vid):random --property 1:KID:$(echo $key_aud) --global-option mpeg-cenc.eme-pssh:true /var/www/html/videos/high_config.mp4 /var/www/html/$(echo $user)/high-config-enc.mp4
 
+mp4dash --input-file /var/www/html/$(echo $user)/high_config-enc.mp4 --input-file /var/www/html/$(echo $user)/med_config-enc.mp4 --input-file /var/www/html/$(echo $user)/low_config-enc.mp4 --mpd-name /var/www/html/$(echo $user)/stream.mpd
