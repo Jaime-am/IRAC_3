3a37275b5276a6413d70860d09daa4d4a577a5a6
Jaime-am <46279145+Jaime-am@users.noreply.github.com>
2023-05-13 22:05:39 +0200

    Update gen_code.sh

--- gen_code.sh
+++ gen_code.sh
@@ -12,9 +12,10 @@ sudo mysql -u jaime <<-EOF
           UPDATE usuarios SET decoder_aud = '$(echo $key_dec_aud)' WHERE user = '$(echo $user)';
 EOF
 
+sudo rm -r /var/www/html/$(echo $user)
 sudo mkdir /var/www/html/$(echo $user)
-bash -c "/Bento4-SDK-1-6-0-639.x86_64-unknown-linux/bin/mp4encrypt --method MPEG-CENC --key 1:$(echo $key_vid):random --property 1:KID:$(echo $key_aud) --global-option mpeg-cenc.eme-pssh:true /var/www/html/videos/low_config.mp4 /var/www/html/$(echo $user)/low-config-enc.mp4
-bash -c "/Bento4-SDK-1-6-0-639.x86_64-unknown-linux/bin/mp4encrypt --method MPEG-CENC --key 1:$(echo $key_vid):random --property 1:KID:$(echo $key_aud) --global-option mpeg-cenc.eme-pssh:true /var/www/html/videos/med_config.mp4 /var/www/html/$(echo $user)/med-config-enc.mp4
-bash -c "/Bento4-SDK-1-6-0-639.x86_64-unknown-linux/bin/mp4encrypt --method MPEG-CENC --key 1:$(echo $key_vid):random --property 1:KID:$(echo $key_aud) --global-option mpeg-cenc.eme-pssh:true /var/www/html/videos/high_config.mp4 /var/www/html/$(echo $user)/high-config-enc.mp4
+sudo bash -c "/Bento4-SDK-1-6-0-639.x86_64-unknown-linux/bin/mp4encrypt --method MPEG-CENC --key 1:$(echo $key_vid):random --property 1:KID:$(echo $key_aud) --global-option mpeg-cenc.eme-pssh:true /var/www/html/videos/low_config.mp4 /var/www/html/$(echo $user)/low-config-enc.mp4"
+sudo bash -c "/Bento4-SDK-1-6-0-639.x86_64-unknown-linux/bin/mp4encrypt --method MPEG-CENC --key 1:$(echo $key_vid):random --property 1:KID:$(echo $key_aud) --global-option mpeg-cenc.eme-pssh:true /var/www/html/videos/med_config.mp4 /var/www/html/$(echo $user)/med-config-enc.mp4"
+sudo bash -c "/Bento4-SDK-1-6-0-639.x86_64-unknown-linux/bin/mp4encrypt --method MPEG-CENC --key 1:$(echo $key_vid):random --property 1:KID:$(echo $key_aud) --global-option mpeg-cenc.eme-pssh:true /var/www/html/videos/high_config.mp4 /var/www/html/$(echo $user)/high-config-enc.mp4"
 
-mp4dash --input-file /var/www/html/$(echo $user)/high_config-enc.mp4 --input-file /var/www/html/$(echo $user)/med_config-enc.mp4 --input-file /var/www/html/$(echo $user)/low_config-enc.mp4 --mpd-name /var/www/html/$(echo $user)/stream.mpd
+cd /var/www/html/$(echo $user) && sudo bash -c "/Bento4-SDK-1-6-0-639.x86_64-unknown-linux/bin/mp4dash /var/www/html/$(echo $user)/high-config-enc.mp4 /var/www/html/$(echo $user)/med-config-enc.mp4 /var/www/html/$(echo $user)/low-config-enc.mp4"
