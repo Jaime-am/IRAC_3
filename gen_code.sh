cb0fd751d3fa366c5ae876acecccaa61507c99cb
Jaime-am <46279145+Jaime-am@users.noreply.github.com>
2023-05-13 20:55:38 +0200

    Update gen_code.sh

--- gen_code.sh
+++ gen_code.sh
@@ -6,6 +6,11 @@ key_aud=$(openssl rand -hex 16)
 key_dec_vid=$(echo $key_vid | xxd -r -p | base64)
 key_dec_aud=$(echo $key_aud | xxd -r -p | base64)
 
+sudo mysql -u jaime <<-EOF
+          use irac;
+          INSERT INTO usuarios (decoder_vid,decoder_aud) VALUES ('$(echo $key_dec_vid)', '$(echo $key_dec_aud)') WHERE user = '$(echo $user)';
+EOF
+
 sudo mkdir /var/www/html/$(echo $user)
 bash -c "/Bento4-SDK-1-6-0-639.x86_64-unknown-linux/bin/mp4encrypt --method MPEG-CENC --key 1:$(echo $key_vid):random --property 1:KID:$(echo $key_aud) --global-option mpeg-cenc.eme-pssh:true /var/www/html/videos/low_config.mp4 /var/www/html/$(echo $user)/low-config-enc.mp4
 bash -c "/Bento4-SDK-1-6-0-639.x86_64-unknown-linux/bin/mp4encrypt --method MPEG-CENC --key 1:$(echo $key_vid):random --property 1:KID:$(echo $key_aud) --global-option mpeg-cenc.eme-pssh:true /var/www/html/videos/med_config.mp4 /var/www/html/$(echo $user)/med-config-enc.mp4
