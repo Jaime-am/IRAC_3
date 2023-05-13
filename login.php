ba622cdde5b42c929806c3787c406aa9a7c7d3f6
Jaime-am <46279145+Jaime-am@users.noreply.github.com>
2023-05-13 17:58:01 +0200

    Update login.php

--- login.php
+++ login.php
@@ -5,6 +5,18 @@ $dbhost = "localhost";
 $dbuser = "jaime";
 $dbpass = "";
 $dbname = "irac";
+ubuntu@ip-10-0-1-60:/var/www/html$ cat login.php | xclip -selection clip
+Error: Can't open display: (null)
+ubuntu@ip-10-0-1-60:/var/www/html$ sudo cat login.php | xclip -selection clip
+Error: Can't open display: (null)
+ubuntu@ip-10-0-1-60:/var/www/html$ cat login.php 
+<?php
+
+//Cargamos variables para entablar conexion con mysql
+$dbhost = "localhost";
+$dbuser = "jaime";
+$dbpass = "";
+$dbname = "irac";
 
 //Trataremos la conexion con mysql como una variable: conn
 $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
@@ -12,7 +24,7 @@ $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 //Si por alguna razon no entabla conexion, nos lo indica con el error en concreto
 if (!$conn) 
 {
-	die("No hay conexión: ".mysqli_connect_error());
+        die("No hay conexión: ".mysqli_connect_error());
 }
 
 //Cargamos variables del form del login para realizar las consultas a la BD
@@ -39,14 +51,14 @@ $nr = 0;
 //En caso de login exitoso
 if($nr == 1)
 {
-    setcookie("decoder", $user_login, time()+3600, "/"); // suponiendo que el nombre de la cookie es "decoder" y se establece por una hora, es la clave del usuario
-	header("Location: index.html");
-	
+//    setcookie("decoder", $user_login, time()+3600, "/"); // suponiendo que el nombre de la cookie es "decoder" y se establece por una hora, es la clave del usuario
+        header("Location: index.html");
+
 }
 //En caso de login no exitoso
 else if ($nr == 0) 
 {
-	header("Location: login.html");
+        header("Location: login.html");
 }
-	
+
 ?>
