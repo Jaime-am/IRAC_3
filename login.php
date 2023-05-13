b51cd0aabdb76a1d47871d4c48548fbf6cbb1aab
Jaime-am <46279145+Jaime-am@users.noreply.github.com>
2023-05-13 17:54:04 +0200

    Update login.php

--- login.php
+++ login.php
@@ -20,7 +20,7 @@ $user_login = $_POST["user_login"];
 $pass_login = $_POST["password_login"];
 $decoder        = $_POST["decoder"];
 $user_signup = $_POST["user_signup"];
-$pass_signup = $_POST["pass_signup"];
+$password_signup = $_POST["password_signup"];
 $nombre        = $_POST["nombre"];
 $apellido = $_POST["apellido"];
 
@@ -32,7 +32,7 @@ $query = mysqli_query($conn,"SELECT * FROM usuarios WHERE user = '".$user_login.
 $nr = mysqli_num_rows($query);
 }
 else if ($user_login == null && $user_signup != null) {
-$query = mysqli_query($conn,"INSERT INTO usuarios (nombre, apellido, user, password) VALUES ('".$nombre."', '".$apellido."', '".$user_signup."', '".$pass_signup."')");
+$query = mysqli_query($conn,"INSERT INTO usuarios (nombre, apellido, user, password) VALUES ('".$nombre."', '".$apellido."', '".$user_signup."', '".$password_signup."')");
 $nr = 0;
 }
 
